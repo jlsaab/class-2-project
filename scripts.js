bb19862d8621013bb5cd51a8db3354e721e75d22
Jessica Saab <93544068+jlsaab@users.noreply.github.com>
2024-03-28 19:35:28 -0400

    Port jsfiddle into html, css, and js files

---
+++ scripts.js
@@ -0,0 +1,34 @@
+$('.card').on('click', function () {
+    var place = $(this).data('place')
+
+    $('#column-right').empty()
+
+    if (place === 'idaho') {
+        $('#column-right').css('background-image', 'url("https://www.hubblehomes.com/images/headerimage/mountain-home-idaho-new-homes-hubble-homes-01.jpg")')
+    }
+
+    if (place === 'italy') {
+        $('#column-right').css('background-image', 'url("https://www.igotravelnetwork.com/immagini/articoli_itinerario/large/79_382_shutterstock_1136337089.jpg")')
+    }
+
+    if (place === 'nj') {
+        $('#column-right').css('background-image', 'url("https://photos.zillowstatic.com/fp/d3210d9e288fe5fb2447544a8d80162f-p_e.jpg")')
+    }
+
+    if (place === 'fl') {
+        $('#column-right').css('background-image', 'url("https://us1-photo.nextdoor.com/post_photos/e6/72/e6720edbd6309e907ca5332dba4b0731.jpg")')
+    }
+
+    if (place === 'spain') {
+        $('#column-right').css('background-image', 'url("https://cycling-friendly.com/wp-content/uploads/2023/04/san-javier-cycling-friendly-000.jpg")')
+    }
+    if (place === 'germany') {
+        $('#column-right').css('background-image', 'url("https://us.images.westend61.de/0001365228pw/germany-baden-wurttemberg-kongen-aerial-view-of-countryside-highway-at-springtime-sunset-WDF05967.jpg")')
+    }
+    if (place === 'tx') {
+        $('#column-right').css('background-image', 'url("https://visit-abilene.s3.amazonaws.com/images/abilene.jpg?v=1650071420")')
+    }
+    if (place === 'nyc') {
+        $('#column-right').css('background-image', 'url("https://assets.bwbx.io/images/users/iqjWHBFdfxIU/iU.dprk0DbdQ/v1/-1x-1.jpg")')
+    }
+})
