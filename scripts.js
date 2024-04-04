ab3b0bed9e7d74ed28ce97928d371167313d5df2
Jessica Saab <93544068+jlsaab@users.noreply.github.com>
2024-04-04 03:45:17 -0400

    Second attempt at updating website
    Changed some HTML, added a bit of JS, and changed majority of CSS

--- scripts.js
+++ scripts.js
@@ -1,24 +1,24 @@
-$('.card').on('click', function () {
+// $('button').click(function () {
+//     $('#column-right').updateClass(".active").fadeToggle();
+// })
+
+$('button').on('click', function () {
     var place = $(this).data('place')
 
-    $('#column-right').empty()
+    $('#column-right').empty().css("background-color", "rgba(37, 49, 84, 0.7)").fadeToggle("slow")
 
     if (place === 'idaho') {
         $('#column-right').css('background-image', 'url("https://www.hubblehomes.com/images/headerimage/mountain-home-idaho-new-homes-hubble-homes-01.jpg")')
     }
-
     if (place === 'italy') {
         $('#column-right').css('background-image', 'url("https://www.igotravelnetwork.com/immagini/articoli_itinerario/large/79_382_shutterstock_1136337089.jpg")')
     }
-
     if (place === 'nj') {
         $('#column-right').css('background-image', 'url("https://photos.zillowstatic.com/fp/d3210d9e288fe5fb2447544a8d80162f-p_e.jpg")')
     }
-
     if (place === 'fl') {
         $('#column-right').css('background-image', 'url("https://us1-photo.nextdoor.com/post_photos/e6/72/e6720edbd6309e907ca5332dba4b0731.jpg")')
     }
-
     if (place === 'spain') {
         $('#column-right').css('background-image', 'url("https://cycling-friendly.com/wp-content/uploads/2023/04/san-javier-cycling-friendly-000.jpg")')
     }
